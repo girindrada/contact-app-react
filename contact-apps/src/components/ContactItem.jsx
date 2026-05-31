4ecb2922bdd4ad9ab4c5ef244e8e925e52af1868
girindra <gdwiixa@gmail.com>
2026-05-31 14:29:49 +0700

    feat: add delete contact handler

--- contact-apps/src/components/ContactItem.jsx
+++ contact-apps/src/components/ContactItem.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ContactItemBody from "./ContactItemBody";
 import ContacItemImage from "./ContactItemImage";
+import DeleteButton from "./DeleteButton";
 
-function ContactItem({ name, tag, imageUrl }) {
+function ContactItem({ name, tag, imageUrl, id, onDelete }) {
   return (
     <div className="contact-item">
       <ContacItemImage imageUrl={imageUrl} />
       <ContactItemBody name={name} tag={tag} />
+      <DeleteButton id={id} onDelete={onDelete} />
     </div>
   );
 }
